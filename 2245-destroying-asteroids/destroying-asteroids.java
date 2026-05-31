785cefe6eb4e77aaca1d2ecfb5c70cc244e04eae
Durga Neha <nehakollipara169@gmail.com>
2026-06-01 00:41:33 +0530

    Time: 26 ms (81.52%) | Memory: 107.5 MB (30.95%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long currentmass = mass;
+        for(int i = 0; i < asteroids.length; i++){
+            if(asteroids[i] > currentmass){
+                return false;
+            }
+            currentmass += asteroids[i];
+        }
+        return true;
+    }
+}
